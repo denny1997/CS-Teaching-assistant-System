d2b5ebc231ff9eb745a2c6fe904e21326c56b336
tianchi3 <tianchi3@illinois.edu>
2020-03-21 23:49:53 -0500

    commit

--- CS_teaching_record_system/controller.py
+++ CS_teaching_record_system/controller.py
@@ -1,13 +1,18 @@
 from Model.models import Course, Faculty, Teaches
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseServerError
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+from django.forms.models import model_to_dict
 
 #index page
 def index(request):
     return render(request, 'index.html')
 
 # Course related
+@csrf_exempt
 def add_course(request):
     if request.POST:
         name = request.POST['name']
@@ -18,21 +23,32 @@ def add_course(request):
         course = Course(name=name,credits=credits,sectionNumber=sectionNum,description=description)
         course.save()
 
-        return HttpResponse("<p>Add course successfully!!</p>")
+        response = HttpResponse("Add course successfully!!")
     else:
-        return HttpResponse("<p>Add course failed!!</p>")
+        response = HttpResponseServerError("Add course failed!!")
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 def add_course_form(request):
     return render(request, 'add_course.html')
 
 def list_course(request):
-    ctx = {}
     course_list = Course.objects.all()
+    d = []
+    for i in course_list:
+        d.append(model_to_dict(i))
+    response = JsonResponse(d, safe=False)
 
-    ctx['list'] = course_list
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
-    return render(request,'list_course.html',ctx)
-
+@csrf_exempt
 def modify_course(request):
     if request.POST:
         id = request.POST['id']
@@ -48,9 +64,14 @@ def modify_course(request):
         course.sectionNumber = sectionNum
         course.description = description
         course.save()
-        return HttpResponse("<p>Modify Course Information Successfully!!</p>")
+        response = HttpResponse("Modify Course Information Successfully!!")
     else:
-        return HttpResponse("<p>Modify fail!!</p>")
+        response = HttpResponseServerError("Modify fail!!")
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 def modify_course_form(request):
     ctx = {}
@@ -61,16 +82,23 @@ def modify_course_form(request):
     else:
         return HttpResponse("<p>Modify fail!!</p>")
 
+
 def delete_course(request):
     if 'course_id' in request.GET and request.GET['course_id']:
         course = Course.objects.get(id=int(request.GET['course_id']))
         course.delete()
-        return HttpResponse("<p>Delete Course Successfully!!</p>")
+        response = HttpResponse("Delete Course Successfully!!")
     else:
-        return HttpResponse("<p>Delete Course Failed!!</p>")
+        response = HttpResponseServerError("Delete Course Failed!!")
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 
 # faculty related
+@csrf_exempt
 def add_faculty(request):
     if request.POST:
         name = request.POST['name']
@@ -80,21 +108,38 @@ def add_faculty(request):
         faculty = Faculty(name=name,email=email,research_area=research)
         faculty.save()
 
-        return HttpResponse("<p>Add faculty successfully!!</p>")
+        response = HttpResponse("Add faculty successfully!!")
     else:
-        return HttpResponse("<p>Add faculty failed!!</p>")
+        response = HttpResponseServerError("Add faculty failed!!")
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 def add_faculty_form(request):
     return render(request, 'add_faculty.html')
 
 def list_faculty(request):
-    ctx = {}
+    # ctx = {}
+    # faculty_list = Faculty.objects.all()
+    #
+    # ctx['list'] = faculty_list
+    #
+    # return render(request,'list_faculty.html',ctx)
     faculty_list = Faculty.objects.all()
+    d = []
+    for i in faculty_list:
+        d.append(model_to_dict(i))
+    response = JsonResponse(d, safe=False)
 
-    ctx['list'] = faculty_list
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
-    return render(request,'list_faculty.html',ctx)
-
+@csrf_exempt
 def modify_faculty(request):
     if request.POST:
         id = request.POST['id']
@@ -108,9 +153,16 @@ def modify_faculty(request):
         faculty.email = email
         faculty.research_area = research
         faculty.save()
-        return HttpResponse("<p>Modify Faculty Information Successfully!!</p>")
+
+        response = HttpResponse("Modify Faculty Information Successfully!!")
     else:
-        return HttpResponse("<p>Modify fail!!</p>")
+        response = HttpResponseServerError("Modify fail!!")
+
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 def modify_faculty_form(request):
     ctx = {}
@@ -121,14 +173,22 @@ def modify_faculty_form(request):
     else:
         return HttpResponse("<p>Modify fail!!</p>")
 
+
 def delete_faculty(request):
     if 'faculty_id' in request.GET and request.GET['faculty_id']:
         faculty = Faculty.objects.get(id=int(request.GET['faculty_id']))
         faculty.delete()
-        return HttpResponse("<p>Delete Faculty Successfully!!</p>")
+        response = HttpResponse("Delete Faculty Successfully!!")
     else:
-        return HttpResponse("<p>Delete Faculty Failed!!</p>")
+        response = HttpResponseServerError("Delete Faculty Failed!!")
 
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
+
+@csrf_exempt
 def add_teaches(request):
     if request.POST:
         course_name = request.POST['course_name']
@@ -139,19 +199,35 @@ def add_teaches(request):
         if faculty.exists():
             faculty_id = faculty.first()
         else:
-            return HttpResponse("<p>faculty not found!!</p>")
+            response = HttpResponseServerError("faculty not found!!")
+            response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+            response["Access-Control-Max-Age"] = "1000"
+            response["Access-Control-Allow-Headers"] = "*"
+            return response
 
         course = Course.objects.filter(name=course_name)
         if course.exists():
             course_id = course.first()
         else:
-            return HttpResponse("<p>course not found!!</p>")
+            response = HttpResponseServerError("course not found!!")
+            response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+            response["Access-Control-Max-Age"] = "1000"
+            response["Access-Control-Allow-Headers"] = "*"
+            return response
 
         teaches = Teaches(semester=semester,course=course_id,faculty=faculty_id)
         teaches.save()
-        return HttpResponse("<p>Add Teaches Successfully!!</p>")
+        response = HttpResponse("Add Teaches Successfully!!")
     else:
-        return HttpResponse("<p>Add Teaches fail!!</p>")
+        response = HttpResponseServerError("Add Teaches fail!!")
+
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 def add_teaches_form(request):
     ctx = {}
@@ -163,8 +239,8 @@ def add_teaches_form(request):
         ctx['semester']=request.GET['semester']
     return render(request, 'add_teaches.html',ctx)
 
+
 def list_teaches(request):
-    ctx = {}
     teaches_list = Teaches.objects.all()
     if 'course_name' in request.GET and request.GET['course_name']:
         course = Course.objects.get(name=request.GET['course_name'])
@@ -177,28 +253,62 @@ def list_teaches(request):
     if 'semester' in request.GET and request.GET['semester']:
         teaches_list = teaches_list.filter(semester=request.GET['semester'])
 
-    ctx['list'] = teaches_list
+    d = []
+    for i in teaches_list:
+        temp={}
+        temp['id']=i.id
+        temp['semester']=i.semester
 
-    return render(request,'list_teaches.html',ctx)
+        temp_course={}
+        temp_course['name']=i.course.name
+        temp_course['credits'] = i.course.credits
+        temp_course['sectionNumber'] = i.course.sectionNumber
+        temp_course['description'] = i.course.description
+        temp['course']=temp_course
+
+        temp_faculty={}
+        temp_faculty['name']=i.faculty.name
+        temp['faculty']=temp_faculty
+
+        d.append(temp)
+    response = JsonResponse(d, safe=False)
+
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
+
 
 def delete_teaches(request):
     if 'teaches_id' in request.GET and request.GET['teaches_id']:
         teaches = Teaches.objects.get(id=int(request.GET['teaches_id']))
         teaches.delete()
-        return HttpResponse("<p>Delete Teaches Successfully!!</p>")
+        response = HttpResponse("Delete Teaches Successfully!!")
     else:
-        return HttpResponse("<p>Delete Teaches Failed!!</p>")
+        response = HttpResponseServerError("Delete Teaches Failed!!")
+
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 def list_free_faculty(request):
-    ctx = {}
     faculty_list = Faculty.objects.all()
     teaches_list = Teaches.objects.all()
     if 'semester' in request.GET and request.GET['semester']:
         teaches_list = teaches_list.filter(semester=request.GET['semester'])
-        ctx['semester'] = request.GET['semester']
 
     faculty_list = faculty_list.exclude(id__in=teaches_list.values_list('faculty',flat=True).distinct())
 
-    ctx['list'] = faculty_list
+    d = []
+    for i in faculty_list:
+        d.append(model_to_dict(i))
+    response = JsonResponse(d, safe=False)
 
-    return render(request,'list_free_faculty.html',ctx)
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
